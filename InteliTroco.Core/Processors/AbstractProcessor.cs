b4de983ab8a1a36299c442ef1e45618e92c0f3b6
eduardojonssen <eduardojonssen@gmail.com>
2016-03-01 11:09:56 -0300

    Implementado o padrão Factory.

---
+++ InteliTroco.Core/Processors/AbstractProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteliTroco.Core.Processors {
+
+	public abstract class AbstractProcessor {
+
+		public virtual IEnumerable<int> GetUnitList() {
+
+			string key = "mu." + this.GetName().Replace("Processor", string.Empty).ToLower();
+			string[] availableUnitList = ConfigurationManager.AppSettings[key].Split(',');
+
+			foreach (string unit in availableUnitList) {
+				yield return int.Parse(unit);
+			}
+
+		}
+
+		public abstract Dictionary<int, long> Process(long amount);
+
+		public virtual string GetName() {
+			return this.GetType().Name;
+		}
+
+	}
+}
